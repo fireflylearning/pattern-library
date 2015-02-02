e19976d479ab273d68e64c109d478b4cf466e489
Jon Roobottom <roo@roodesign.co.uk>
2015-02-02 11:15:59 +0000

    Adding Messages

--- _dev/_htmlhead.php
+++ _dev/_htmlhead.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="/css/styles.css" type="text/css" media="screen" charset="utf-8">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/zenburn.min.css">
     <title>Firefly Core Pattern Library. Beta.</title>
   </head>
--- _dev/_nav.php
+++ _dev/_nav.php
@@ -1,8 +1,8 @@
 <nav>
 	<ul>
 		<li><a href="/">All Patterns</a></li>
-		<li><a href="/manage-set-tasks.php">Manage Set Tasks Example</a></li>
-		<li><a href="/bookmarks.php">Bookmarks Example</a></li>
-		<li><a href="/your-tasks.php">Your Tasks Example</a></li>
+		<li><a href="/_dev/manage-set-tasks.php">Manage Set Tasks Example</a></li>
+		<li><a href="/_dev/bookmarks.php">Bookmarks Example</a></li>
+		<li><a href="/_dev/your-tasks.php">Your Tasks Example</a></li>
 	</ul>
 </nav>
