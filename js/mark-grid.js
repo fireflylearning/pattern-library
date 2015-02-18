6df71a54fd6b0f41ecc20acde65a30c8d6bc9650
Jon Roobottom <roo@roodesign.co.uk>
2015-02-18 11:19:54 +0000

    Adding more markbook goodness

--- js/mark-grid.js
+++ js/mark-grid.js
@@ -1,24 +1,138 @@
-var ffMarkGrid__markCurrentTask = function(obj) {
+//temp js to help with markup...
+
+/*
+    ---
+    isSmall: checks if the markbook should be in mobile mode
+    ---    
+*/
+
+var ffMarkGrid__isSmall = function(obj)
+{
+    var breakpoint = 800;
     
-    //adds a class of 'ff-mark-grid-active' to <th> and <th> elements
-    //that match the content of 'data-ff-mark-grid-task' 
-    //required for responisveness
-    
-    var currentTask = obj.attr('data-ff-mark-grid-task');
-    
-    //mark results
-    $('td[headers]',obj).each(function() {
-        $that = $(this);
-        $that.removeClass('ff-mark-grid-active');
-        if($that.attr('headers') == currentTask) {
-            $that.addClass('ff-mark-grid-active');  
-        };
-    });
+    if(obj.width() <= breakpoint)
+    {
+        obj.addClass('ff-mark-book-small');
+    }
+    else
+    {
+        obj.removeClass('ff-mark-book-small');
+    } 
+};
+
+/*
+    ---
+    fixHeader: checks if the .ff-mark-grid-header objects should be fixed to the top of the page
+    ---    
+*/
+
+var ffMarkGrid__fixHeader = function(obj)
+{
+  
+  
+  $(window).on("scroll.ffMarkGridScroll",function()
+  {
+      if(!obj.hasClass('ff-mark-book-small'))
+      {
+            var aboveHeight = obj.offset().top;
+            var objHeight = obj.outerHeight(false);	 
+            if ($(window).scrollTop() > aboveHeight && $(window).scrollTop() < (aboveHeight + objHeight)) 
+            {
+                obj.addClass('ff-mark-grid-fixed-header');
+            }
+            else
+            {
+                obj.removeClass('ff-mark-grid-fixed-header');
+            }
+      }
+      else
+      {
+          $(window).off('.ffMarkGridScroll');
+      }
+  });
     
 };
 
-$(function() {
-    $('.ff-mark-grid').each(function() {
-        ffMarkGrid__markCurrentTask($(this));
-    });  
+
+/*
+    ---
+    ffPopover__show: show this popover!
+    ---    
+*/
+
+var ffPopover__show = function(obj)
+{
+    
+    //firstly hide any popovers / kill js-generated popovers
+    ffPopover__hide();
+    
+    
+    var popover = $('.ff-popover',obj);
+    
+    //now, either clone a template popover OR show this in place
+    if(popover.hasClass('ff-popover-js-template'))
+    {
+        var offset = obj.offset();
+        var height = obj.height();
+        var width = popover.width();
+        
+        var top = offset.top + height;
+        var left = offset.left - ((width/2) - 30);
+        
+        console.log('left',offset.left,'top',offset.top,'height',height,'width',width);
+        popover.clone().appendTo('body').addClass('ff-popover-js-generated').css({
+            'top' : top,
+            'left' : left 
+        }).fadeIn();
+    }
+}
+
+var ffPopover__hide = function()
+{
+    $('.ff-popover').hide();
+    $('.ff-popover-js-generated').remove();
+}
+
+
+
+/*
+    ---
+    Call when dom is ready / resize event fires.
+    If you keep any of this js, you should chnage these events to use the 
+    Firefly event firing API.
+    ---    
+*/
+
+$(function() 
+{
+    
+    $('.ff-mark-grid').each(function() 
+    {
+        $that = $(this);
+        ffMarkGrid__isSmall($that);
+        ffMarkGrid__fixHeader($that);
+    }); 
+    
+    $( window ).resize(function()
+    {
+        $('.ff-mark-grid').each(function() 
+        {
+            $that = $(this);
+            ffMarkGrid__isSmall($that);
+            ffMarkGrid__fixHeader($that);
+        }); 
+    });
+    
+    
+    $('.ff-popover-trigger').on("click",function() 
+    {
+        ffPopover__show($(this));
+    });
+    $('body').on("click",function(e) 
+    {
+        if ($(e.target).closest('.ff-popover-trigger, .ff-popover').length === 0) {
+            ffPopover__hide()
+        }
+    });
+     
 });
