cd1e7132471fc68ed72197f15a462e219b724f15
Jon Roobottom <roo@roodesign.co.uk>
2015-12-03 09:39:03 +0000

    updates to the date picker

--- blocks/core/ff_module/ff_module-date-picker-button/ff_module-date-picker-button.js
+++ blocks/core/ff_module/ff_module-date-picker-button/ff_module-date-picker-button.js
@@ -4,8 +4,13 @@ var $ = require('jquery');
 
 module.exports = function() {
     $(function() {
-        $('[data-component="date-picker"]').datepicker();
-        $('[data-component="date-picker"]').datepicker('setDate', new Date);
-        // datepicker('hide');
+        $('[data-component="date-picker"]').each(function(index,element) {
+           $(element).datepicker({
+                dateFormat: "yy-mm-dd",
+                onSelect: function(date) {
+                    location.href = $(element).attr("data-url-prefix") + date;
+                }
+            }); 
+        });
     });
 };
