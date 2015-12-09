64f57478e7d6f67ce63c4e46dd35ba7e1ac07f23
Danielle Huntrods <daniellehuntrods@gmail.com>
2015-12-09 17:36:04 +0000

    Updating assets for recipient picker

--- blocks/core/_shared/index.js
+++ blocks/core/_shared/index.js
@@ -1,18 +1,10 @@
 'use strict';
-var React = require('react');
+
 
 require('../ff_module/ff_module-button/ff_module-button')();
 require('../ff_module/ff_module-filter/ff_module-filter')();
 require('../ff_module/ff_module-tabs-navigation/ff_module-tabs-navigation')();
 
-// var editFac = require('../ff_module/ff_module-inline-edit/stubControl.js');
-// require('../ff_module/ff_module-inline-edit/ff_module-inline-edit')(editFac);
+require('../ff_module/ff_module-inline-help/_ff_module-inline-help-renderer')();
 
-var helpFac = require('../ff_module/ff_module-inline-help/stubControl.js');
-require('../ff_module/ff_module-inline-help/ff_module-inline-help')(helpFac);
 
-var PeoplePicker = require('../ff_module/ff_module-recipient-picker/ff_module-recipient-picker');
-
-var peoplePickerControl = require('../ff_module/ff_module-recipient-picker/ff_module-recipient-picker.control.js')();
-var peoplePicker = PeoplePicker.createPeoplePicker(peoplePickerControl);
-React.render(React.createElement(peoplePicker), document.getElementById('container'));
