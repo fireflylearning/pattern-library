9c854b83653d8163e821df43e48d6e809e4f1ca4
Jon Roobottom <roo@roodesign.co.uk>
2015-12-21 10:41:53 +0000

    A few fixes for a number of modules.

--- blocks/core/ff_module/ff_module-filter/ff_module-filter.js
+++ blocks/core/ff_module/ff_module-filter/ff_module-filter.js
@@ -6,10 +6,12 @@ module.exports = function() {
     $(function() {
         $("[data-ff-action='filter-control']").click(function() {
             var filterDropdown = $(this),
-                filterIcon = filterDropdown.find("[data-ff-icon='filter-dropdown-icon']"),
+                filterIcon = filterDropdown.find("[data-ff-icon-open]"),
+                filterIconOpen = filterIcon.attr("data-ff-icon-open"),
+                filterIconClosed = filterIcon.attr("data-ff-icon-closed"),
                 filterContent = filterDropdown.closest("[data-ff='filter']").find("[data-ff='filter-content']");
 
-            filterIcon.toggleClass("ff_icon-arrow--closed");
+            filterIcon.toggleClass(filterIconOpen + " " + filterIconClosed);
             filterContent.slideToggle(200);
         });
     });
