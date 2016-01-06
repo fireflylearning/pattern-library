9198d8fb228ce7b106b5e670757866a5c6d07d82
Jon Roobottom <roo@roodesign.co.uk>
2016-01-06 18:34:26 +0000

    jump to datepicker styling update and js fix to force picker away from the edge of the window

--- blocks/core/ff_module/ff_module-date-picker-jumpto/ff_module-date-picker-jumpto.js
+++ blocks/core/ff_module/ff_module-date-picker-jumpto/ff_module-date-picker-jumpto.js
@@ -3,6 +3,7 @@ var $ = require('jquery');
 
 module.exports = function() {
     $(function() {
+        $.extend($.datepicker,{_checkOffset:function(inst,offset,isFixed){return offset}});
         return $('[data-ff="date-picker"]').each(function(index, element) {
             $(element).datepicker({
                 dateFormat: "yy-mm-dd",
@@ -11,6 +12,18 @@ module.exports = function() {
                 },
                 beforeShow: function(input, inst) {
                     $(inst.dpDiv).addClass('ff_module-date-picker-calendar');
+                    setTimeout(function () {
+                        var curr_left = inst.dpDiv.css('left');
+                        var curr_right = inst.dpDiv.css('right');
+
+                        var new_left = 30 + parseInt(curr_left);
+                        var new_right = 30 + parseInt(curr_right);
+
+                        console.log(new_left,new_right);
+
+                        if(curr_left != 'auto') {inst.dpDiv.css('left',new_left);}
+                        if(curr_right != 'auto') {inst.dpDiv.css('right',new_right);}
+                    }, 0);
                 }
             })
         });
