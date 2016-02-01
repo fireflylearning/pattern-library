7b9e9fced54c7a5647354f6d96b931efdf2f3b63
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-02-01 16:52:31 +0000

    Adding jumpto back in

--- blocks/core/_shared/index.js
+++ blocks/core/_shared/index.js
@@ -7,9 +7,9 @@ require('../ff_module/ff_module-recipient-picker/ff_module-recipient-picker-comp
 require('../ff_module/ff_module-recipient-picker-new-group-button/_ff_module-recipient-picker-new-group-button-renderer')();
 require('../ff_module/ff_module-recipient-button-list/ff_module-recipient-button-list-component/_ff_module-recipient-button-list-component-renderer')();
 
+require('../ff_module/ff_module-date-picker-jumpto/ff_module-date-picker-jumpto')();
 require('../ff_module/ff_module-tabs/_ff_module-tabs-renderer')();
 require('../ff_module/ff_module-formsteps/_ff_module-formsteps-renderer')();
-
 require('../ff_module/ff_module-inline-help/_ff_module-inline-help-renderer')();
 
 require('../lib_test/lib_test-recipient-picker/_lib_test-recipient-picker-renderer')();
