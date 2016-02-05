848d66e6c3f4983d9b52cd156ad97b93d0695c38
Michael Williamson <michael@fireflylearning.com>
2016-02-05 13:54:55 +0000

    Add key when wrapping ff_container-item-repeater

--- blocks/core/ff_container/ff_container-item-repeater/_ff_container-item-repeater.rt.js
+++ blocks/core/ff_container/ff_container-item-repeater/_ff_container-item-repeater.rt.js
@@ -2,7 +2,10 @@
 var React = require('react/addons');
 var _ = require('lodash');
 function repeatChild1(child, childIndex) {
-    return React.createElement('li', { 'className': 'ff_container-item-repeater__item' }, '\n            ', child, '\n        ');
+    return React.createElement('li', {
+        'className': 'ff_container-item-repeater__item',
+        'key': child.key
+    }, '\n            ', child, '\n        ');
 }
 module.exports = function () {
     return React.createElement('div', { 'className': 'ff_container-item-repeater' }, React.createElement.apply(this, [
