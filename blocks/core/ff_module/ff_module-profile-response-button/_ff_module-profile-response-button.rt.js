9095e7f9a5ac4ff4c18dc51349490cf509beca8c
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-02-16 17:17:07 +0000

    Renaming profile button text value from 'title' to 'label'

--- blocks/core/ff_module/ff_module-profile-response-button/_ff_module-profile-response-button.rt.js
+++ blocks/core/ff_module/ff_module-profile-response-button/_ff_module-profile-response-button.rt.js
@@ -10,5 +10,5 @@ module.exports = function () {
     }, React.createElement('figure', { 'className': 'ff_module-profile-response-button__picture' }, React.createElement('img', {
         'className': 'ff_module-profile-response-button__image',
         'src': this.props.pic_href
-    })), React.createElement('div', { 'className': 'ff_module-profile-response-button__caption' }, React.createElement('span', { 'className': 'ff_module-profile-response-button__title' }, this.props.label), React.createElement('span', { 'className': 'ff_module-profile-response-button__status' }, this.props.status), React.createElement('span', { 'className': 'ff_module-profile-response-button__mark' }, this.props.mark)));
+    })), React.createElement('div', { 'className': 'ff_module-profile-response-button__caption' }, React.createElement('span', { 'className': 'ff_module-profile-response-button__label' }, this.props.label), React.createElement('span', { 'className': 'ff_module-profile-response-button__status' }, this.props.status), React.createElement('span', { 'className': 'ff_module-profile-response-button__mark' }, this.props.mark)));
 };
