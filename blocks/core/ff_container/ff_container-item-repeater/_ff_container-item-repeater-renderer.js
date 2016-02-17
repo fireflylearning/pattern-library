dac82bd1f5e8921314daab61e5ef707f1655a229
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-02-17 15:37:37 +0000

    Adding modifiers to container-item-repeater

--- blocks/core/ff_container/ff_container-item-repeater/_ff_container-item-repeater-renderer.js
+++ blocks/core/ff_container/ff_container-item-repeater/_ff_container-item-repeater-renderer.js
@@ -1,16 +1,18 @@
 'use strict';
 var React = require('react');
 
-
 var ItemRepeater = require('./ff_container-item-repeater');
+var data = [{key:'item1', text: 'Item 1'}, {key:'item2', text: 'Item 2'}, {key:'item3', text: 'Item 3'}];
 
 module.exports = function() {
     document.addEventListener('DOMContentLoaded', function(event) {
         var el = document.querySelector('[data-ff-container-item-repeater]'); //Use jquery or sim in Firefly for backwards compat
-        // var el = document.getElementById('recipient-picker-container');
         if (el) {
-            var repeater = React.render(React.createElement(ItemRepeater), el);
-            // picker.getSelectedRecipients();
+            var repeater = React.createElement(ItemRepeater, { modifier: 'separated' }, data.map(function(datum, index) {
+                return React.createElement('p', { key: datum.key, style: { margin: 0 } }, datum.text);
+            }));
+
+            React.render(repeater, el);
         }
     });
 };
--- blocks/core/ff_container/ff_container-item-repeater/ff_container-item-repeater.md
+++ blocks/core/ff_container/ff_container-item-repeater/ff_container-item-repeater.md
@@ -1,9 +1,16 @@
 ---
 data:
-  -
     items:
       - "item 1"
       - "item 2"
       - "item 3"
       - "item 4"
 ---
+
+## React component (`separated`):
+<div data-ff-container-item-repeater=""/>
+
+## Modifiers
+- `rounded` (Also default): rounded borders on outside of list, borders between list items
+- `separated`: no borders on outside of list, only borders between list items
+- `squared`: square borders on outside of list, borders between list items
