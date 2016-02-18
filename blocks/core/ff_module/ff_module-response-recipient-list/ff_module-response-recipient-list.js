3fe799b368bd0e318a4b74e5d697a4d7d383ac8a
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-02-18 17:07:38 +0000

    Adding displayNames and keys

--- blocks/core/ff_module/ff_module-response-recipient-list/ff_module-response-recipient-list.js
+++ blocks/core/ff_module/ff_module-response-recipient-list/ff_module-response-recipient-list.js
@@ -4,5 +4,6 @@ var React = require('react');
 var template = require('./_ff_module-response-recipient-list.rt.js');
 
 module.exports = React.createClass({
+    displayName: 'TaskResponseRecipientList',
     render: template
 });
--- blocks/core/ff_module/ff_module-task-event-repeater/_ff_module-task-event-repeater.rt.js
+++ blocks/core/ff_module/ff_module-task-event-repeater/_ff_module-task-event-repeater.rt.js
@@ -3,7 +3,10 @@ var React = require('react/addons');
 var _ = require('lodash');
 var TaskEvent = require('../../ff_module/ff_module-task-event/ff_module-task-event.js');
 function repeatEvent1(event, eventIndex) {
-    return React.createElement('li', { 'className': 'ff_container-task-event-repeater__item' }, React.createElement(TaskEvent, { 'event': event }));
+    return React.createElement('li', {
+        'className': 'ff_container-task-event-repeater__item',
+        'key': 'event-' + eventIndex
+    }, React.createElement(TaskEvent, { 'event': event }));
 }
 module.exports = function () {
     return React.createElement('div', { 'className': 'ff_container-task-event-repeater' }, React.createElement.apply(this, [
