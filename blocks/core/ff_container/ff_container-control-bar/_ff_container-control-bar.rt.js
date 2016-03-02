7620061c6df2013b822b780cb8127048a3e3fdd1
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-03-02 11:30:24 +0000

    Updating ContainerControlBar

--- blocks/core/ff_container/ff_container-control-bar/_ff_container-control-bar.rt.js
+++ blocks/core/ff_container/ff_container-control-bar/_ff_container-control-bar.rt.js
@@ -1,16 +1,18 @@
 'use strict';
 var React = require('react/addons');
 var _ = require('lodash');
+var ControlBarSet = require('./_src/_ControlBarSet.js');
 function repeatSet1(set, setIndex) {
-    return React.createElement('div', {
-        'className': 'ff_container-control-bar__group',
+    return React.createElement(ControlBarSet, {
+        'title': set.title,
         'key': setIndex
-    }, set.title ? React.createElement('h3', { 'className': 'ff_container-control-bar__title' }, set.title) : null, set.modules);
+    }, set.modules);
 }
 module.exports = function () {
     return React.createElement.apply(this, [
         'div',
         { 'className': 'ff_container-control-bar' + (this.props.modifier ? ' ff_container-control-bar--' + this.props.modifier : '') + (this.props.classes ? ' ' + this.props.classes : '') },
-        _.map(this.props.sets, repeatSet1.bind(this))
+        _.map(this.props.sets, repeatSet1.bind(this)),
+        this.props.children
     ]);
 };
---
+++ blocks/core/ff_container/ff_container-control-bar/_src/_ControlBarSet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var React = require('react');
+var template = require('./_ControlBarSet.rt.js');
+
+module.exports = React.createClass({
+    displayName: 'ControlBarSet',
+    render: template
+});
