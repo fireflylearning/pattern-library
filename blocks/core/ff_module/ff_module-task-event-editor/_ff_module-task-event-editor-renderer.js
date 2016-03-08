9391ebd5be2d54e50acf9bf8ffa6168c3dc64869
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-03-08 11:38:30 +0000

    Replicate stub Task Event Editor component

--- blocks/core/ff_module/ff_module-task-event-editor/_ff_module-task-event-editor-renderer.js
+++ blocks/core/ff_module/ff_module-task-event-editor/_ff_module-task-event-editor-renderer.js
@@ -3,13 +3,53 @@
 var React = require('react');
 
 var TaskEventEditor = require('./ff_module-task-event-editor');
+var eventTypes = require('../ff_module-task-event/_src/events').types;
+var dStrings = ['27 Feb 2016 03:24:00', '27 Feb 2016 03:28:00', '28 Feb 2016 13:24:00'];
+
+var events = [{
+    type: eventTypes.stampResponseAsSeen,
+    sent: new Date(dStrings[1]),
+    author: { name: 'Terry Teacher' },
+    message: 'Message to the student'
+}, {
+    type: eventTypes.comment,
+    sent: new Date(dStrings[2]),
+    author: { name: 'Terry Teacher' },
+    comment: 'Much better, this sets the essay up very well. Very good character analysis, you understand the different perspectives and explained the context very thoroughly. Keep up the good work!',
+    message: 'Much better, this sets the essay up very well. Very good character analysis, you understand the different perspectives and explained the context very thoroughly. Keep up the good work!'
+}, {
+    type: eventTypes.requestResubmission,
+    sent: new Date(dStrings[0]),
+    error: true,
+    author: { name: 'Terry Teacher' }
+}, {
+    type: eventTypes.confirmTaskIsComplete,
+    sent: new Date(dStrings[1]),
+    author: { name: 'Terry Teacher' }
+}, {
+    type: eventTypes.confirmStudentIsExcused,
+    sent: new Date(dStrings[2]),
+    author: { name: 'Terry Teacher' }
+}, {
+    type: eventTypes.markAndGrade,
+    sent: new Date(dStrings[0]),
+    author: { name: 'Terry Teacher' },
+    mark: 7,
+    markMax: 10,
+    grade: 'B'
+}];
 
 module.exports = function() {
     document.addEventListener('DOMContentLoaded', function(evnt) {
         Array.prototype.forEach.call(document.querySelectorAll('[data-ff_module-task-event-editor]'), function(domElement, index) {
 
-            var element = React.createElement(TaskEventEditor);
-            React.render(element, domElement);
+            var root = React.createElement('ul', { style: { listStyle: 'none', margin: 0, padding: 0 } }, events.map(function(event) {
+                return React.createElement('li', { style: { listStyle: 'none', margin: 0, padding: 0, marginBottom: '5px' } },
+                    React.createElement(TaskEventEditor, {event: event, onSend(){
+                        console.log('send');
+                    }}));
+            }));
+            React.render(root, domElement);
         });
     });
 };
--- blocks/core/ff_module/ff_module-task-event-editor/ff_module-task-event-editor.js
+++ blocks/core/ff_module/ff_module-task-event-editor/ff_module-task-event-editor.js
@@ -1,11 +1,111 @@
 'use strict';
 
-var React = require('react');
+var React = require('react'),
+    _ = require('lodash');
+
+var Button = require('../ff_module-button/ff_module-button'),
+    eventTypes = require('../ff_module-task-event/_src/events').types;
 
 
 module.exports = React.createClass({
     displayName: 'TaskEventEditor',
     render: function() {
-        return React.createElement('div', {}, 'Task Event Editor');
+        console.log(eventEditorComponents[this.props.event.type]);
+        var eventEditor = eventEditorComponents[this.props.event.type](this.props);
+        return React.createElement('div', {}, [
+            React.createElement('h1', {}, eventEditor.title),
+            eventEditor.children,
+            React.createElement(Button, { text: eventEditor.sendText, onClick: this.onSend })
+        ]);
+    },
+    onSend: function() {
+        this.props.onSend();
     }
 });
+
+
+function createEventWithMessageEditor(editor) {
+    var messageLabelElement = editor.messageLabel ? React.createElement('p', {}, editor.messageLabel) : null;
+
+    return function(props) {
+        var onMessageChange = function(event) {
+            props.onChange(_.extend({}, props.event, { message: event.target.value }));
+        };
+
+        return {
+            title: editor.title,
+            sendText: editor.sendText,
+            children: React.createElement('div', {}, [
+                messageLabelElement,
+                React.createElement('textarea', { onChange: onMessageChange, value: props.event.message })
+            ])
+        };
+
+    };
+}
+
+function markAndGrade(props) {
+
+    function eventUpdater(propertyName) {
+        return function(event) {
+            props.onChange(_.extend({}, props.event, {
+                [propertyName]: event.target.value }));
+        };
+    }
+
+    var onMarkChange = eventUpdater("mark");
+    var onMarkMaxChange = eventUpdater("markMax");
+    var onGradeChange = eventUpdater("grade");
+    var onMessageChange = eventUpdater("message");
+
+    return {
+        title: "Mark and Grade",
+        sendText: "Add Mark",
+        children: React.createElement('div', {}, [
+            React.createElement('p', {}, [
+                'Mark: ',
+                React.createElement('input', { value: props.event.mark, onChange: onMarkChange }),
+                ' out of ',
+                React.createElement('input', { value: props.event.markMax, onChange: onMarkMaxChange }),
+            ]),
+            React.createElement('p', {}, [
+                'Grade: ',
+                React.createElement('input', { value: props.event.grade, onChange: onGradeChange })
+            ]),
+            React.createElement('p', {}, 'Feedback Summary (optional)'),
+            React.createElement('textarea', { value: props.event.message, onChange: onMessageChange })
+
+        ]),
+
+
+
+    };
+}
+
+var eventEditorComponents = {
+    [eventTypes.stampResponseAsSeen]: createEventWithMessageEditor({
+        title: "Stamp as Seen",
+        messageLabel: "Feedback (optional)",
+        sendText: "Send Stamp"
+    }),
+    [eventTypes.requestResubmission]: createEventWithMessageEditor({
+        title: "Request Resubmission",
+        messageLabel: "Reason for Request (optional)",
+        sendText: "Send Request"
+    }),
+    [eventTypes.confirmTaskIsComplete]: createEventWithMessageEditor({
+        title: "Confirm Task is Complete",
+        messageLabel: "Feedback (optional)",
+        sendText: "Send Confirmation"
+    }),
+    [eventTypes.confirmStudentIsExcused]: createEventWithMessageEditor({
+        title: "Confirm Student is Excused",
+        messageLabel: "Comment (optional)",
+        sendText: "Send Confirmation"
+    }),
+    [eventTypes.comment]: createEventWithMessageEditor({
+        title: "Comment",
+        sendText: "Add Comment"
+    }),
+    [eventTypes.markAndGrade]: markAndGrade
+};
