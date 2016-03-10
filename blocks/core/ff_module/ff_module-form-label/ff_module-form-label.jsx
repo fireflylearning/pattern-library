e2c5ae10f037153005baf130d661399b629713cb
Adam Lynam <adam@fireflylearning.com>
2016-03-10 15:17:43 +0000

    Correcting mistake using props.to instead of props.id

--- blocks/core/ff_module/ff_module-form-label/ff_module-form-label.jsx
+++ blocks/core/ff_module/ff_module-form-label/ff_module-form-label.jsx
@@ -4,9 +4,9 @@ var React = require('react');
 
 module.exports = React.createClass({
     render() {
-        return <label htmlFor={this.props.to} className={'ff_module-form-label' + (this.props.modifier != null ? ' ff_module-form-label--' + this.props.modifier : '')} {...this.props.data}>
+        return <label htmlFor={this.props.id} className={'ff_module-form-label' + (this.props.modifier != null ? ' ff_module-form-label--' + this.props.modifier : '')} {...this.props.data}>
             {this.props.children}
             {(!this.props.required) ? <span className="ff_module-form-label__optional"> (optional)</span> : ''}
         </label>;
     }
-}
+});
