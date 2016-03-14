fb9a897ac1772e4bd5cb18c85b0b319c9bb2fed0
Simone De Luca <simone@fireflylearning.com>
2016-03-14 11:34:59 +0000

    React for filter container

---
+++ blocks/core/ff_container/ff_container-filter/_ff_container-filter-renderer.js
@@ -0,0 +1,24 @@
+'use strict';
+var React = require('react');
+
+var FilterContainer = require('./ff_container-filter');
+
+var data = {
+	label: 'Sort by',
+	modifier: 'fixed-height',
+	modules: [{
+		content: <span className={'crate_util-block'}>Module A</span>
+	},
+	{
+		content: <span className={'crate_util-block'}>Module B</span>
+	},
+	{
+		content: <span className={'crate_util-block'}>Module C</span>	
+	}]
+}
+
+module.exports = function() {
+	document.addEventListener('DOMContentLoaded', function(event) {
+		React.render(React.createElement(FilterContainer, data), document.querySelector('[data-ff_container-filter]'));
+	});
+};
---
+++ blocks/core/ff_container/ff_container-filter/ff_container-filter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+	displayName: 'FilterContainer',
+	render: function() {
+		return <div className={'ff_container-filter'}>
+			<div className={'ff_container-filter__heading'}>
+				<label className={'ff_container-filter__label'}>{this.props.label}</label>
+			</div>
+			<div className={'ff_container-filter__items' + (this.props.modifier ? ' ff_container-filter__items--' + this.props.modifier : '')}>
+				{this.props.modules.map(function(module){
+					return module.content;
+				})}
+			</div>
+		</div>;	
+	}
+});
