3ad9c76f4082170c1e024c873dd4576c22767d5c
Simone De Luca <simone@fireflylearning.com>
2016-03-14 16:01:34 +0000

    updates following Danielle's comments

--- blocks/core/ff_container/ff_container-filter/ff_container-filter.js
+++ blocks/core/ff_container/ff_container-filter/ff_container-filter.js
@@ -5,9 +5,9 @@ var React = require('react');
 module.exports = React.createClass({
 	displayName: 'FilterContainer',
 	render: function() {
-		return <div className={'ff_container-filter'}>
-			<div className={'ff_container-filter__heading'}>
-				<label className={'ff_container-filter__label'}>{this.props.label}</label>
+		return <div className='ff_container-filter'>
+			<div className='ff_container-filter__heading'>
+				<label className='ff_container-filter__label'>{this.props.label}</label>
 			</div>
 			<div className={'ff_container-filter__items' + (this.props.modifier ? ' ff_container-filter__items--' + this.props.modifier : '')}>
 				{this.props.filters.map(function(filter){
--- tests/ff_container-filter.js
+++ tests/ff_container-filter.js
@@ -32,4 +32,22 @@ describe('FilterContainer', function() {
 		var items = TestUtils.scryRenderedDOMComponentsWithTag(component, 'span');
 		expect(items.length).to.equal(filters.length);
 	});
+
+	var label= "[Sort by]";
+	it('should render a filter container with a label ' + label, function() {
+		var element = React.createElement(FilterContainer, { label: label, filters: [] } );
+		var component = TestUtils.renderIntoDocument(element);
+
+		var labelNode = TestUtils.findRenderedDOMComponentWithClass(component, 'ff_container-filter__label');
+		expect(labelNode.textContent).to.equal(label);
+	});
+
+	var modifier = '[Modifier]';
+	it('should render a filter container with modifier ' + label, function() {
+		var element = React.createElement(FilterContainer, { modifier: modifier, filters: [] } );
+		var component = TestUtils.renderIntoDocument(element);
+
+		var itemsNode = TestUtils.findRenderedDOMComponentWithClass(component, 'ff_container-filter__items');
+		expect(itemsNode.className).to.equal('ff_container-filter__items ff_container-filter__items--' + modifier);
+	});
 });
