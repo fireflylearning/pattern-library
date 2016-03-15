fa7908cdc1c916faf163e0e3d23c5624ea5bfe21
Simone De Luca <simone@fireflylearning.com>
2016-03-15 13:34:00 +0000

    checkable list to react

--- blocks/core/ff_module/ff_module-form-checkable-list/_ff_module-form-checkable-list-renderer.js
+++ blocks/core/ff_module/ff_module-form-checkable-list/_ff_module-form-checkable-list-renderer.js
@@ -4,19 +4,39 @@ var React = require('react');
 var CheckableList = require('./ff_module-form-checkable-list');
 
 var data = {
-	modifier: 'inline',
-	inputs: [{
-		label: 'Radio item 1',
+	items: [{
+		label: 'Label text',
+		required: true,
+		modifier: 'inline',
 		type: 'radio',
-		id: 'radio-id-1',
+		id: 'react-radio-id-1',
 		value: 'radio-value-1',
 		name: 'radio-list-group',
-		checked: false
+		key: '1'
+	},
+	{	label: 'Label text',
+		required: true,
+		type: 'radio',
+		modifier: 'inline',
+		id: 'react-radio-id-2',
+		value: 'radio-value-2',
+		name: 'radio-list-group',
+		key: '2'
+	},
+	{	
+		label: 'Label text',
+		required: true,
+		type: 'radio',
+		modifier: 'inline',
+		id: 'react-radio-id-3',
+		value: 'radio-value-3',
+		name: 'radio-list-group',
+		key: '3'
 	}]
-}
+};
 
 module.exports = function() {
 	document.addEventListener('DOMContentLoaded', function(event) {
-		React.render(React.createElement(CheckableList, data), document.querySelector('[data-ff_module-form-checkable-list]'));
+		React.render(<CheckableList {...data} />, document.querySelector('[data-ff_module-form-checkable-list]'));
 	});
 };
