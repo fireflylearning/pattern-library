9e1723f15d509ceb89cdc037c4debcc388e4c73c
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-03-16 17:36:36 +0000

    Update modules -button and -form-label

--- blocks/core/ff_module/ff_module-form-label/ff_module-form-label.jsx
+++ blocks/core/ff_module/ff_module-form-label/ff_module-form-label.jsx
@@ -4,9 +4,11 @@ var React = require('react');
 
 module.exports = React.createClass({
     render() {
+        var optionalMarkerSet = !(this.props.optionalMarker === null || this.props.optionalMarker === undefined);
+        var optionalMarker = optionalMarkerSet ? this.props.optionalMarker : '(optional)';
         return <label htmlFor={this.props.id} className={'ff_module-form-label' + (this.props.modifier != null ? ' ff_module-form-label--' + this.props.modifier : '')} {...this.props.data}>
             {this.props.children}
-            {(!this.props.required) ? <span className="ff_module-form-label__optional"> (optional)</span> : ''}
+            {(!this.props.required) ? <span className="ff_module-form-label__optional"> {optionalMarker}</span> : ''}
         </label>;
     }
 });
