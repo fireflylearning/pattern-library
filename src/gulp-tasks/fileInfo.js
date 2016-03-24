73966c0d6dac1d19cb8dc0b6473d2d3516e25199
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-03-24 22:55:17 +0000

    Ensure sub-block files don't override block files

--- src/gulp-tasks/fileInfo.js
+++ src/gulp-tasks/fileInfo.js
@@ -56,25 +56,45 @@ function getFileData(filePath) {
     return fileData;
 }
 
-function getBlockFileProcess(block, options) {
+function ifClosestBlockMatchesCurrentBlock(filePath, currentBlock, dataStore, cb) {
+    var dirs = filePath.split(path.sep),
+        closestBlock;
+    dirs.pop();
+    dirs.reverse(); // Start with last dirs to find closest block
+    async.detect(dirs, function(dirName, cb) {
+        closestBlock = dataStore.getBlock(dirName);
+        if (closestBlock) return cb(true);
+        cb(false);
+    }, function(result) {
+        if (closestBlock === currentBlock) return cb(true);
+        cb(false);
+    });
+}
+
+function getBlockFileProcess(block, dataStore, options) {
     return function processFile(filePath, fileCallback) {
-        var fileData = getFileData(filePath),
-            file = block.addFile(fileData.fsInfo.name, fileData.fsInfo.resolvedName);
+        var fileData, file;
 
-        if (options.generateFileUrl) {
-            file.setUrlPath(options.generateFileUrl(fileData.fsInfo.name, fileData.fsInfo.resolvedName))
-        }
+        ifClosestBlockMatchesCurrentBlock(filePath, block, dataStore, function(blocksMatch) {
+            if (blocksMatch) {
+                fileData = getFileData(filePath);
+                file = block.addFile(fileData.fsInfo.name, fileData.fsInfo.resolvedName);
 
-        file.addMeta(fileData.meta);
-        file.addData(fileData.data);
-        file.addContent(fileData.content);
+                if (options.generateFileUrl) {
+                    file.setUrlPath(options.generateFileUrl(fileData.fsInfo.name, fileData.fsInfo.resolvedName));
+                }
 
-        block.addMeta(fileData.meta);
-        block.addData(fileData.data);
-        block.addDependencies(fileData.requires);
+                file.addMeta(fileData.meta);
+                file.addData(fileData.data);
+                file.addContent(fileData.content);
 
-        fileCallback();
-    }
+                block.addMeta(fileData.meta);
+                block.addData(fileData.data);
+                block.addDependencies(fileData.requires);
+            }
+            fileCallback();
+        });
+    };
 }
 
 function getFileProcess(pageData, options) {
@@ -83,7 +103,7 @@ function getFileProcess(pageData, options) {
             file = pageData.addFile(fileData.fsInfo.name, fileData.fsInfo.resolvedName);
 
         if (options.generateFileUrl) {
-            file.setUrlPath(options.generateFileUrl(fileData.fsInfo.name, fileData.fsInfo.resolvedName))
+            file.setUrlPath(options.generateFileUrl(fileData.fsInfo.name, fileData.fsInfo.resolvedName));
         }
         // console.log('[fileInfo]', fileData.data);
         file.addMeta(fileData.meta);
@@ -91,34 +111,36 @@ function getFileProcess(pageData, options) {
         file.addContent(fileData.content);
 
         fileCallback();
-    }
+    };
 }
 
-function getFileIterator(fileFilter, processFile, processFileComplete, generateUrl) {
+function getFileIterator(fileFilter, processFile, processFileComplete) {
 
     return function(err, files) {
         if (err) return processFileComplete(err);
-        files = files.filter(fileFilter);
-        async.each(files, processFile, processFileComplete);
-    }
+
+        async.filter(files, fileFilter, function(results) {
+            async.each(results, processFile, processFileComplete);
+        });
+    };
 }
 
 function getBlockSubdirProcess(dataStore, options) {
     return function processSubdir(blockPath, subdirCallback) {
         var blockData = getBlockData(blockPath),
             block = dataStore.addBlock(blockData.blockName, blockData.resolvedBlockName),
-            processFile = getBlockFileProcess(block, options),
+            processFile = getBlockFileProcess(block, dataStore, options),
             processFileComplete = function(err) {
                 if (err) return subdirCallback(err);
                 subdirCallback();
             };
 
         if (options.generateBlockUrl) {
-            block.setUrlPath(options.generateBlockUrl(blockData.blockName, blockData.resolvedBlockName))
+            block.setUrlPath(options.generateBlockUrl(blockData.blockName, blockData.resolvedBlockName));
         }
 
         dir.files(blockPath, getFileIterator(options.filterFile, processFile, processFileComplete));
-    }
+    };
 }
 
 function getProcessSubdirComplete(dataStore, callback) {
@@ -127,11 +149,11 @@ function getProcessSubdirComplete(dataStore, callback) {
         // console.log('\n__________________');
         // console.log(util.inspect(dataStore.getAllData(), {
         //     showHidden: false,
-        //     depth: null
+        //     depth: 2
         // }));
         // console.log('\n__________________');
         callback();
-    }
+    };
 }
 
 function getSubDirIterator(processSubdir, processSubdirComplete, options) {
@@ -143,7 +165,7 @@ function getSubDirIterator(processSubdir, processSubdirComplete, options) {
             async.each(results, processSubdir, processSubdirComplete);
         });
 
-    }
+    };
 }
 
 function getBlockInfo(fileSrc, dataStore, options) {
@@ -168,4 +190,4 @@ function getFileInfo(fileSrc, dataStore, options) {
 module.exports = {
     getBlockInfo: getBlockInfo,
     getFileInfo: getFileInfo
-}
+};
