b26c751a88075c338af99ee9010ae8172f74d9a3
Simone De Luca <simone@fireflylearning.com>
2016-04-13 15:18:15 +0100

    Included marking-controls and added styles

--- blocks/core/ff_module/ff_module-form-preview/_ff_module-form-preview-renderer.js
+++ blocks/core/ff_module/ff_module-form-preview/_ff_module-form-preview-renderer.js
@@ -2,6 +2,48 @@
 var React = require('react');
 
 var FormPreview = require('./ff_module-form-preview');
+var MarkingControls = require('../ff_module-form-marking-controls/ff_module-form-marking-controls');
+
+var controlsProps = {
+	controls: {
+		buttons: [{
+		text:'Send Reminder',
+		id: 1,
+		key: 1
+		},
+		{
+			text: 'Mark Now',
+			id: 2,
+			key: 2
+		},
+		{
+			text: 'Release Marks',
+			id: 3,
+			key: 3
+		}],
+		checkableList: {
+			modifier: 'stacked',
+			items: [{
+				label: 'Label text',
+				required: true,
+				type: 'radio',
+				id: 'react-radio-id-1',
+				value: 'radio-value-1',
+				name: 'react-radio-list-group',
+				key: 1
+			},
+			{
+				label: 'Label text',
+				required: true,
+				type: 'radio',
+				id: 'react-radio-id-2',
+				value: 'radio-value-2',
+				name: 'react-radio-list-group',
+				key: 2
+			}]
+		}
+	}
+}
 
 var data = {
 	items: [{
@@ -36,7 +78,8 @@ var data = {
 			no_excused: 2,
 			completed_by: 20,
 			marked: 3
-		}
+		},
+		markingControls: true	
 	},
 	{
 		title: 'Description',
@@ -48,7 +91,7 @@ module.exports = function() {
 	document.addEventListener('DOMContentLoaded', function(event) {
 		var element = document.querySelector('[data-ff_module-form-preview]');
 		if (element) {
-			React.render(<FormPreview {...data} />, element);
+			React.render(<FormPreview {...data} controls={<MarkingControls {...controlsProps}/>}/>, element);
 		}
 	});
 };
--- blocks/core/ff_module/ff_module-form-preview/ff_module-form-preview.js
+++ blocks/core/ff_module/ff_module-form-preview/ff_module-form-preview.js
@@ -23,24 +23,34 @@ module.exports = React.createClass({
 			<ul className='ff_module-form-preview__list'>
 				{this.props.items.map((item)=>{
 					var isInlineEdit = item.url ?  <span className='ff_module-form-preview__edit-link'><InlineEdit url={item.url} text='Edit'></InlineEdit></span> : '';
-					var listItemData;
+					var isAdditionalInfo = item.progress ?  'ff_module-form-preview__item--additional-info' : '';
+					var itemData;
 					
-					if (item.list) {
-						listItemData = <dl className='ff_module-form-preview__sublist'>
-							{item.list.map((item)=>{
-								return [<dt className='ff_module-form-preview__sublist__title'>{item.title}</dt>,
-								<dd className='ff_module-form-preview__sublist__data' data-ff-preview-for={item.previewfor}>{item.value}</dd>]
-							})}
-						</dl>;
-					} else if(item.html) {
-						listItemData = <div className='ff_module-form-preview__list__description'>{item.html}</div>
-					} else if(item.progress){
-						listItemData = <Progress {...item.progress}/>;
-					} else {
-						listItemData = item.value;
+					switch(true) {
+						case !!item.list:
+							itemData = <dl className='ff_module-form-preview__sublist'>
+								{item.list.map((item)=>{
+									return [<dt className='ff_module-form-preview__sublist__title'>{item.title}</dt>,
+									<dd className='ff_module-form-preview__sublist__data' data-ff-preview-for={item.previewfor}>{item.value}</dd>];
+								})}
+							</dl>;
+							break;
+						case !!item.html:
+							itemData = <div className='ff_module-form-preview__list__description'>{item.html}</div>;
+							break;
+						case !!item.progress:
+							itemData = <div><div className="ff_module-form-preview__progress"><Progress {...item.progress}/></div> <div className='ff_module-form-preview__controls'>{!!item.markingControls ? this.props.controls : ''}</div></div>;
+							break;
+						default:
+							itemData = item.value;
+
 					}
-					return <li className='ff_module-form-preview__item'><dl><dt className='ff_module-form-preview__list__title'><span className='ff_module-form-preview__list__title__text'>{item.title}</span>{isInlineEdit}</dt>
-						<dd className='ff_module-form-preview__list__data' data-ff-preview-for={item.previewfor}>{listItemData}</dd></dl></li>;
+					return <li className={'ff_module-form-preview__item ' + isAdditionalInfo}>
+						<dl>
+							<dt className='ff_module-form-preview__list__title'><span className='ff_module-form-preview__list__title__text'>{item.title}</span>{isInlineEdit}</dt>
+						<dd className='ff_module-form-preview__list__data' data-ff-preview-for={item.previewfor}>{itemData}</dd>
+						</dl>
+					</li>;
 				})}
 			</ul>
 		</div>;
