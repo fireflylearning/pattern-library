1b0c5caf510b64d722a50b452ffcb9bda472af24
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-04-25 16:58:19 +0100

    Adding state matrix to task event status

--- blocks/core/ff_module/ff_module-task-event-status/_ff_module-task-event-status-renderer.js
+++ blocks/core/ff_module/ff_module-task-event-status/_ff_module-task-event-status-renderer.js
@@ -4,26 +4,30 @@ var React = require('react'),
 
 var TaskEventStatus = require('./ff_module-task-event-status');
 var eventStates = require('../ff_module-task-event/_src/events').states;
-var eventTypes = require('../ff_module-task-event/_src/events').events;
+var eventTypes = require('../ff_module-task-event/_src/events').types;
 
-var dStrings = ['27 Feb 2016 03:24:00', '27 Feb 2016 03:28:00', '28 Feb 2016 13:24:00'];
+var m1States = [
+    eventStates.default,
+    eventStates.deleted
+];
+var m2States = [
+    eventStates.error,
+    eventStates.pending,
+    eventStates.released,
+    eventStates.unreleased,
+    eventStates.saved
+];
 
-var errorEvent = {};
-errorEvent[eventStates.error] = true;
-
-var pendingEvent = {};
-pendingEvent[eventStates.pending] = true;
-
-var releasedEvent = {};
-releasedEvent[eventStates.released] = true;
-
-var unreleasedEvent = {};
-unreleasedEvent[eventStates.unreleased] = true;
-
-var savedEvent = {};
-savedEvent[eventStates.saved] = true;
-
-var events = [errorEvent, pendingEvent, releasedEvent, unreleasedEvent, savedEvent];
+var types = [
+    eventTypes.setTask,
+    eventTypes.stampResponseAsSeen,
+    eventTypes.requestResubmission,
+    eventTypes.confirmStudentIsExcused,
+    eventTypes.confirmTaskIsComplete,
+    eventTypes.comment,
+    eventTypes.markAndGrade,
+    eventTypes.addFile
+];
 
 // var statuses = [eventStates.default, eventStates.pending, eventStates.error, eventStates.saved, eventStates.released, eventStates.unreleased];
 
@@ -32,10 +36,19 @@ module.exports = function() {
 
         Array.prototype.forEach.call(document.querySelectorAll('[data-ff_module-task-event-status]'), function(domElement, index) {
             var root = <ul style={{ listStyle: 'none', margin: 0, padding: '5px', backgroundColor:'#fff' }}>
-                {events.map(function(event, index){
-                    return <li key={'li'+index} style={{ listStyle: 'none', margin: 0, padding: 0, marginBottom: '5px' }}>
-                    <TaskEventStatus event={event} />
-                    </li>;
+                {m1States.map((m1State, m1Index)=>{
+                    return m2States.map((m2State, m2Index)=>{
+                        return types.map((type, tIndex)=>{
+                            var state = {};
+                            state[m1State] = true;
+                            state[m2State] = true;
+
+                            return <li key={'li'+m1State+''+m2State+''+tIndex} style={{ listStyle: 'none', margin: 0, padding: 0, marginBottom: '5px' }}>
+                                <pre>{type}</pre>
+                                <TaskEventStatus state={state} type={type}/>
+                                </li>;
+                        })
+                    })
                 })}
             </ul>;
 
--- blocks/core/ff_module/ff_module-task-event-status/ff_module-task-event-status.js
+++ blocks/core/ff_module/ff_module-task-event-status/ff_module-task-event-status.js
@@ -8,31 +8,48 @@ var stateClasses = require('../ff_module-task-event/_src/events').stateClasses;
 var dStrings = ['27 Feb 2016 03:24:00', '27 Feb 2016 03:28:00', '28 Feb 2016 13:24:00'];
 
 var messages = {};
-messages[eventStates.default] = '';
-messages[eventStates.released] = '';
-messages[eventStates.pending] = 'Sending';
-messages[eventStates.error] = 'Waiting to Send';
-messages[eventStates.saved] = 'Sent';
-messages[eventStates.unreleased] = 'Ready to Send';
+messages[eventStates.default] = {};
+messages[eventStates.default][eventStates.default] = '';
+messages[eventStates.default][eventStates.released] = '';
+messages[eventStates.default][eventStates.pending] = 'Sending';
+messages[eventStates.default][eventStates.error] = 'Error Sending';
+messages[eventStates.default][eventStates.saved] = 'Sent';
+messages[eventStates.default][eventStates.unreleased] = 'Ready to Send';
 
-function getPresentationState(event) {
-    var state = eventStates.default;
+messages[eventStates.deleted] = {};
+messages[eventStates.deleted][eventStates.default] = '';
+messages[eventStates.deleted][eventStates.released] = '';
+messages[eventStates.deleted][eventStates.pending] = 'Deleting';
+messages[eventStates.deleted][eventStates.error] = 'Error Deleting';
+messages[eventStates.deleted][eventStates.saved] = 'Deleted';
+messages[eventStates.deleted][eventStates.unreleased] = 'Ready to Delete';
 
-    if (event.error) {
-        state = eventStates.error;
-    } else if (event.pending) {
-        state = eventStates.pending;
-    } else if (event.unreleased) {
-        state = eventStates.unreleased;
-    } else if (event.saved) {
-        state = eventStates.saved;
+function getPresentationState(state) {
+    var presentationState = eventStates.default;
+
+    if (state.error) {
+        presentationState = eventStates.error;
+    } else if (state.pending) {
+        presentationState = eventStates.pending;
+    } else if (state.unreleased) {
+        presentationState = eventStates.unreleased;
+    } else if (state.saved) {
+        presentationState = eventStates.saved;
     }
-    return state;
+    return presentationState;
+}
+
+function getM1State(state) {
+    var m1State = eventStates.default;
+    if (state.deleted) {
+        m1State = eventStates.deleted;
+    }
+    return m1State;
 }
 
 function getGeneratedClass(base, props){
     var classNames = [base],
-        type = getPresentationState(props.event);
+        type = getPresentationState(props.state);
     if (!!props.modifier) classNames.push(base + '--' + props.modifier);
     if (!!props.classes) classNames.push(props.classes);
     if (!!props.className) classNames.push(props.className);
@@ -41,12 +58,13 @@ function getGeneratedClass(base, props){
 }
 
 function getStatusMessage(props) {
-    var type = getPresentationState(props.event);
-    return messages[type] || '';
+    var state = getPresentationState(props.state),
+        type = getM1State(props.state);
+    return messages[type] && messages[type][state] || '';
 }
 
 function getIcon(props) {
-    var type = getPresentationState(props.event);
+    var type = getPresentationState(props.state);
     if (type === eventStates.saved) {
         return <span className='ff_icon ff_icon-tick-open-success ff_module-task-event-status__icon'></span>;
     }
@@ -56,9 +74,10 @@ function getIcon(props) {
 module.exports = React.createClass({
     displayName: 'TaskEventStatus',
     propTypes: {
-        event: React.PropTypes.object.isRequired
+        state: React.PropTypes.object.isRequired
     },
     render: function() {
+        console.log(this.props.state);
         return <span className={getGeneratedClass('ff_module-task-event-status', this.props)}>
             {getIcon(this.props)}
             {getStatusMessage(this.props)}
