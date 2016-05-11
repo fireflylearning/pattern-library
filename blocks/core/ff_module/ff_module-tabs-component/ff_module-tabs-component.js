15dce462b031c5cbf6bd616d3cde0e0ec1149837
Simone De Luca <simone@fireflysolutions.co.uk>
2016-05-11 13:03:53 +0100

    removed unused variables

--- blocks/core/ff_module/ff_module-tabs-component/ff_module-tabs-component.js
+++ blocks/core/ff_module/ff_module-tabs-component/ff_module-tabs-component.js
@@ -3,11 +3,6 @@
 var React = require('react');
 var TabsContent = require('../../ff_container/ff_container-tabs-content/ff_container-tabs-content.js');
 var TabsNavigation = require('..//ff_module-tabs-navigation-component/ff_module-tabs-navigation-component.js');
-var TabsControl = require('../ff_module-tabs/ff_module-tabs.js');
-var TabsValidator = require('../ff_module-tabs/_ff_module-tabs-control')();
-
-var targetName = 'data-ff_module-tabs-react-target';
-var contentName = 'data-ff_module-tabs-react-content';
 
 module.exports = React.createClass({
 	displayName: 'TabsComponent',
