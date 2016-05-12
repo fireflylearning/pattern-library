5517c32b7fbfe958cf6991a3e1866dd566dd35bd
Simone De Luca <simone@fireflysolutions.co.uk>
2016-05-12 17:10:57 +0100

    Updated tests to make sure that the onTabSelected is called with the right key

--- tests/ff_module-tabs-component.test.js
+++ tests/ff_module-tabs-component.test.js
@@ -9,14 +9,12 @@ var sinon = require('sinon');
 var TabsComponent = require('../blocks/core/ff_module/ff_module-tabs-component/ff_module-tabs-component');
 
 var tabs = [{
-		state: 'is-active',
 		label: 'test label 1',
 		content: <p>test content 1</p>,
 		id: "tab1a",
 		key: 1
 	},
 	{
-		state: '',
 		label: 'test label 2',
 		content: <p>test content 2</p>,
 		id: "tab2a",
@@ -47,16 +45,12 @@ describe('TabsComponent', function() {
 		expect(root[0].className).to.contain('ff_container-tabs-content ff_container-tabs-content--' + modifier);
 	});
 
-	it('should call the on onTabSelected and select the right tab', function(){
+	it('should call the on onTabSelected with the right key', function(){
 		var element = React.createElement(TabsComponent, {selectedTabKey: 1, onTabSelected: sinon.spy(), tabs:tabs});
 		var component = TestUtils.renderIntoDocument(element);
-		var tabsLi = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_module-tabs-navigation__tab');
 		var tabsLink = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_module-tabs-navigation__link');
 		var clickedTab = tabsLink[1];
 		TestUtils.Simulate.click(clickedTab);
-		expect(element.props.onTabSelected.called).to.be.true;
-		setTimeout(function(){
-			expect(tabsLi[1].className).to.contain('--is-active');
-		}, 100);
+		expect(element.props.onTabSelected.calledWith(2)).to.be.true;
 	});
 });
