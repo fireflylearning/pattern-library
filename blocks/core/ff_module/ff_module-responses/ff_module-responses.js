2d1c3692d69517ffedcfaff090984bb724402518
Simone De Luca <simone@fireflysolutions.co.uk>
2016-05-23 10:54:18 +0100

    Conditionally rendered the smallscreen content below the active element

--- blocks/core/ff_module/ff_module-responses/ff_module-responses.js
+++ blocks/core/ff_module/ff_module-responses/ff_module-responses.js
@@ -8,15 +8,22 @@ var TaskResponses = require('../ff_module-task-responses/ff_module-task-response
 module.exports = React.createClass({
 	render: function() {
 		return <div className='ff_module-responses'>
-			<ItemRepeater modifier="separated ">
-			{this.props.responses.map((response)=>{
-				var isResponseSelected = !!response.isSelected ? <TaskResponses {...response}/> : '';
-				return <div className="ff_module-responses__response">
-					<div className='ff_module-responses__recipient'><ResponseButton {...response}/></div>
-					<div className='ff_module-responses__content'>{isResponseSelected}</div>
-				</div>
-			})}
-			</ItemRepeater>
+			<div className='ff_module-responses__recipients'>
+				<ItemRepeater modifier="separated ">
+				{this.props.responses.map((response)=>{
+					var isSelected = response.isSelected ? <div className=' ff_module-responses__content ff_module-responses__content--smallscreen'><TaskResponses {...response}/></div> : '';
+					return <div>
+						<div className='ff_module-responses__recipient'><ResponseButton {...response}/></div>
+						{isSelected}
+					</div>;
+				})}
+				</ItemRepeater>
+			</div>
+			<div className='ff_module-responses__content'>
+				{this.props.responses.map((response)=>{
+					if (response.isSelected) return  <div><TaskResponses {...response}/></div>;
+				})}
+			</div>
 		</div>
 	}
 });
