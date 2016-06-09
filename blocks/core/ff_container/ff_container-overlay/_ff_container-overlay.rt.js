6e36f20ba61e6c7e144d1a5a207f47514d70d8e0
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-06-09 17:11:53 +0100

    Remove all .rt and associated files

--- blocks/core/ff_container/ff_container-overlay/_ff_container-overlay.rt.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var React = require('react/addons');
-var _ = require('lodash');
-module.exports = function () {
-    return React.createElement('div', { 'className': 'ff_container-overlay' + (this.props.modifier ? ' ff_container-overlay--' + this.props.modifier : '') + (this.props.classes ? ' ' + this.props.classes : '') }, React.createElement('div', { 'className': 'ff_container-overlay__body' }, this.props.body), React.createElement('div', { 'className': 'ff_container-overlay__bar' }, React.createElement('div', { 'className': 'ff_container-overlay__bar-content' }, this.props.bar)));
-};
--- blocks/core/ff_module/ff_module-profile-picture-and-name/ff_module-profile-picture-and-name-button/_ff_module-profile-picture-and-name-button-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require('react');
-var template = require('./_ff_module-profile-picture-and-name-button.rt.js');
-
-module.exports = React.createClass({
-    render: template,
-    displayName: 'ResultButton'
-});
