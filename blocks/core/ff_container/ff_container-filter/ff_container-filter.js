332e03e59c9554af8932b9fb8f9e2d1075a5ff6b
Danielle Huntrods <daniellehuntrods@gmail.com>
2016-06-21 12:36:42 +0100

    Add standard class modifier handlers to container-filter

--- blocks/core/ff_container/ff_container-filter/ff_container-filter.js
+++ blocks/core/ff_container/ff_container-filter/ff_container-filter.js
@@ -1,19 +1,23 @@
 'use strict';
 
 var React = require('react');
+var generateClasses = require('../../_lib/_ui/class-utils.js').generateStandardClass;
+
 
 module.exports = React.createClass({
 	displayName: 'FilterContainer',
 	render: function() {
-		return <div className='ff_container-filter'>
-			<div className='ff_container-filter__heading'>
-				<label className='ff_container-filter__label'>{this.props.label}</label>
-			</div>
-			<div className={'ff_container-filter__items' + (this.props.modifier ? ' ff_container-filter__items--' + this.props.modifier : '')}>
-				{this.props.filters.map(function(filter){
-					return filter.content;
-				})}
-			</div>
-		</div>;	
+		return (
+            <div className={generateClasses('ff_container-filter', this.props)}>
+    			<div className='ff_container-filter__heading'>
+    				<label className='ff_container-filter__label'>{this.props.label}</label>
+    			</div>
+    			<div className={'ff_container-filter__items' + (this.props.modifier ? ' ff_container-filter__items--' + this.props.modifier : '')}>
+    				{this.props.filters.map(function(filter){
+    					return filter.content;
+    				})}
+    			</div>
+    		</div>
+        );
 	}
 });
