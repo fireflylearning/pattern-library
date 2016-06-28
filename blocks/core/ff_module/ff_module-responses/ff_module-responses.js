802e20bab128d41fdf2ce85ca8fe83745b981b43
Simone De Luca <simone@fireflysolutions.co.uk>
2016-06-28 13:52:07 +0100

    Changed responses to accept a list, the response details and the current response as props

--- blocks/core/ff_module/ff_module-responses/ff_module-responses.js
+++ blocks/core/ff_module/ff_module-responses/ff_module-responses.js
@@ -9,20 +9,10 @@ module.exports = React.createClass({
 	render: function() {
 		return <div className='ff_module-responses'>
 			<div className='ff_module-responses__recipients'>
-				<ItemRepeater modifier="separated ">
-				{this.props.responses.map((response)=>{
-					var isSelected = response.isSelected ? <div className=' ff_module-responses__content ff_module-responses__content--smallscreen'><TaskResponses {...response}/></div> : '';
-					return <div>
-						<div className='ff_module-responses__recipient'><ResponseButton {...response}/></div>
-						{isSelected}
-					</div>;
-				})}
-				</ItemRepeater>
+				
 			</div>
 			<div className='ff_module-responses__content'>
-				{this.props.responses.map((response)=>{
-					if (response.isSelected) return  <div><TaskResponses {...response}/></div>;
-				})}
+				
 			</div>
 		</div>
 	}
