0d49276c7b197771cecfa9e958c7fa6b7c48ee2f
Simone De Luca <simone@fireflysolutions.co.uk>
2016-07-08 10:10:23 +0100

    Fixed checkable list warning

--- tests/ff_module-form-checkable-list.test.js
+++ tests/ff_module-form-checkable-list.test.js
@@ -7,29 +7,30 @@ var expect = require('chai').expect;
 
 var CheckableList = require('../blocks/core/ff_module/ff_module-form-checkable-list/ff_module-form-checkable-list');
 
+var items = [{ label: 'item A', id: 'itemA' }, { label: 'item B', id: 'itemB' }, { label: 'item C', id: 'itemC' }];
+var modifier = '[Modifier]';
+
 describe('CheckableList', function() {
 
 	//Checking that component renders correctly
 	it('should render', function() {
-		var element = React.createElement(CheckableList, {items:[]});
+		var element = React.createElement(CheckableList, {items: items});
 		var component = TestUtils.renderIntoDocument(element);
 		expect(component).to.exist;
 	});
 
 	//Checking modifier
-	var modifier = '[Modifier]';
 	it('should make a checkable list item with modifier ' + modifier, function() {
 		var element = React.createElement(CheckableList, {
-            modifier: modifier,
-			items: [{}]
+            		modifier: modifier,
+			items: items
 		});
 		var component = TestUtils.renderIntoDocument(element);
-		var root = TestUtils.findRenderedDOMComponentWithClass(component, 'ff_module-form-checkable-list__item');
-		expect(root.className).to.equal('ff_module-form-checkable-list__item ff_module-form-checkable-list__item--' + modifier);
+		var root = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_module-form-checkable-list__item');
+		expect(root[0].className).to.equal('ff_module-form-checkable-list__item ff_module-form-checkable-list__item--' + modifier);
 	});
 
 	// Checking that it renders the right number of items
-	var items = [{ label: 'item A', id: 'itemA' }, { label: 'item B', id: 'itemB' }, { label: 'item C', id: 'itemC' }];
 	it('should make a checkable list with ' + items.length + ' items', function() {
 		var element = React.createElement(CheckableList, {
 			items: items
