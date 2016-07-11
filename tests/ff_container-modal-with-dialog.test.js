ed304bb3ad6e870f582f080c7b40b876d15570d1
Simone De Luca <simone@fireflysolutions.co.uk>
2016-07-11 17:23:50 +0100

    Final fixes to some weird indentation here

--- tests/ff_container-modal-with-dialog.test.js
+++ tests/ff_container-modal-with-dialog.test.js
@@ -18,18 +18,18 @@ var ContainerModalWithDialog = require('../blocks/core/ff_container/ff_container
 describe('ContainerModalWithDialog', function() {
 
             it('should render', function() {
-                    var element = React.createElement(ContainerModalWithDialog, props[0], < p > [Body text] < /p>);
+                    var element = React.createElement(ContainerModalWithDialog, props[0], <p>[Body text]</p>);
                         var component = TestUtils.renderIntoDocument(element); expect(component).to.exist;
                     });
 
                 it('should have \'getOverlay\' method', function() {
-                        var element = React.createElement(ContainerModalWithDialog, props[0], < p > [Body text] < /p>);
+                        var element = React.createElement(ContainerModalWithDialog, props[0], <p>[Body text]</p>);
                             var component = TestUtils.renderIntoDocument(element); expect(component.getOverlay).to.be.a('function'); expect(component.getOverlay()).to.be.a('object');
                         });
 
                     it('should have render correct components and children', function() {
                         var element = < ContainerModalWithDialog {...props[1] } >
-                            < p > [Body text] < /p> < /ContainerModalWithDialog > ;
+                            <p>[Body text]</p>< /ContainerModalWithDialog > ;
 
                         var component = TestUtils.renderIntoDocument(element);
                         var portal = component.getOverlay();
