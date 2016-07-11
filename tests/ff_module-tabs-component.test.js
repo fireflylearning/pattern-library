0956d423a1b7fbdef2e5d09ba2e52f4fae7ce785
Simone De Luca <simone@fireflysolutions.co.uk>
2016-07-11 09:58:33 +0100

    Fixed tabs component tests

--- tests/ff_module-tabs-component.test.js
+++ tests/ff_module-tabs-component.test.js
@@ -22,31 +22,32 @@ var tabs = [{
 	}
 ];
 var modifier = '[Modifier]';
+var selectedTabKey = 1; 
 
 describe('TabsComponent', function() {
 
 	it('should render a set of tabs', function() {
-		var element = React.createElement(TabsComponent, {tabs: tabs});
+		var element = React.createElement(TabsComponent, {selectedTabKey: selectedTabKey, tabs: tabs});
 		var component = TestUtils.renderIntoDocument(element);
 		expect(component).to.exist;
 	});
 
 	it('should render ' + tabs.length + ' tabs', function() {
-		var element = React.createElement(TabsComponent, {tabs: tabs});
+		var element = React.createElement(TabsComponent, {selectedTabKey: selectedTabKey, tabs: tabs});
 		var component = TestUtils.renderIntoDocument(element);
 		var root = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_module-tabs-navigation__tab');
 		expect(root.length).to.equal(tabs.length);
 	});
 
 	it('should render tabs with a modifier: ' + modifier, function() {
-		var element = React.createElement(TabsComponent, {modifier: modifier, tabs: tabs});
+		var element = React.createElement(TabsComponent, {selectedTabKey: selectedTabKey, modifier: modifier, tabs: tabs});
 		var component = TestUtils.renderIntoDocument(element);
 		var root = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_container-tabs-content');
 		expect(root[0].className).to.contain('ff_container-tabs-content ff_container-tabs-content--' + modifier);
 	});
 
 	it('should call the on onTabSelected with the right key', function(){
-		var element = React.createElement(TabsComponent, {selectedTabKey: 1, onTabSelected: sinon.spy(), tabs:tabs});
+		var element = React.createElement(TabsComponent, {selectedTabKey: selectedTabKey, onTabSelected: sinon.spy(), tabs:tabs});
 		var component = TestUtils.renderIntoDocument(element);
 		var tabsLink = TestUtils.scryRenderedDOMComponentsWithClass(component, 'ff_module-tabs-navigation__link');
 		var clickedTab = tabsLink[1];
